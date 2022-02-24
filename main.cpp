5ea284479ee9a19458180a4f14350d85332afb95
krithikbaghmar <krithik@pysphere.in>
2022-02-24 23:56:17 +0530

    Created a demo module

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include "iostream.h"
+#include "string.h"
+class Skill
+{
+    private:
+        int level;
+        string name;
+        int Role1,Role2;
+
+    public:
+        levelUp()
+        {
+
+        }
+        compare(Role1, Role2)
+        {
+            return bool;
+        }   
+};
+class Contributor
+{
+    private:
+        string name;
+        int noOfSkills;
+        string skills[20];
+        int Role1,Role2;
+    
+    public:
+        bool canMentor(Role1,Role2)
+        {
+            return true;
+        }
+};
+class Role extends Skill
+{
+
+};
+class Project
+{
+    string name;
+    int daysToComplete, score,bestBefore, noOfRoles;
+    string Roles[20];
+};
