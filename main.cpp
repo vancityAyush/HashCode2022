1948e2fddae2866aea9e0c770f5ae29932a10b72
vancityAyush <eliasakeleven@gmail.com>
2022-02-24 23:59:42 +0530

    one

--- main.cpp
+++ main.cpp
@@ -2,42 +2,40 @@
 #include "string.h"
 class Skill
 {
-    private:
-        int level;
-        string name;
-        int Role1,Role2;
+private:
+    int level;
+    string name;
 
-    public:
-        levelUp()
-        {
-
-        }
-        compare(Role1, Role2)
-        {
-            return bool;
-        }   
+public:
+    levelUp()
+    {
+        level++;
+    }
+    compare(Role1, Role2)
+    {
+        return bool;
+    }
 };
 class Contributor
 {
-    private:
-        string name;
-        int noOfSkills;
-        string skills[20];
-        int Role1,Role2;
-    
-    public:
-        bool canMentor(Role1,Role2)
-        {
-            return true;
-        }
+private:
+    string name;
+    int noOfSkills;
+    string skills[20];
+    int Role1, Role2;
+
+public:
+    bool canMentor(Role1, Role2)
+    {
+        return true;
+    }
 };
 class Role extends Skill
 {
-
 };
 class Project
 {
     string name;
-    int daysToComplete, score,bestBefore, noOfRoles;
+    int daysToComplete, score, bestBefore, noOfRoles;
     string Roles[20];
 };
